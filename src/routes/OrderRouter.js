c8dda2f04649b3427658abf9bdcad5263e20fec0
phucdeotrai <tranhoangphuchatien07@gmail.com>
2024-07-29 01:01:53 +0700

    29/7/24 v2

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const OrderController = require('../controllers/OrderController')
 const {authUserMiddleware, authMiddleware} = require("../middleware/authMiddleware")
 
-router.post('/create', authUserMiddleware, OrderController.createOrder)
+router.post('/create', OrderController.createOrder)
 
 router.get('/get-order-details/:id',authUserMiddleware, OrderController.getDetailOrder)
 
